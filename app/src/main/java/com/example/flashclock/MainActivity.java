91c7c077af8e0796cd57ee3e5ec2a47fde7ccf10
quycheese <dtqchii@gmail.com>
2021-10-15 20:40:46 +0700

    FlashClock App

---
+++ app/src/main/java/com/example/flashclock/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.flashclock;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import org.naishadhparmar.zcustomcalendar.CustomCalendar;
+import org.naishadhparmar.zcustomcalendar.Property;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mBottomNavigationView;
+    private ViewPager2 mViewPager;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBottomNavigationView = findViewById(R.id.bottom_nav);
+        mViewPager = findViewById(R.id.view_pager);
+
+
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+        mViewPager.setAdapter(viewPagerAdapter);
+
+        mBottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.action_timer){
+                    mViewPager.setCurrentItem(0);
+                }else if(id == R.id.action_timenation) {
+                    mViewPager.setCurrentItem(1);
+                }else  if(id == R.id.action_presstime) {
+                    mViewPager.setCurrentItem(2);
+                }
+                return true;
+            }
+        });
+        
+       mViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+           @Override
+           public void onPageSelected(int position) {
+               super.onPageSelected(position);
+               switch (position) {
+                   case 0:
+                       mBottomNavigationView.getMenu().findItem(R.id.action_timer).setChecked(true);
+                       break;
+                   case 1:
+                       mBottomNavigationView.getMenu().findItem(R.id.action_timenation).setChecked(true);
+                       break;
+                   case 2:
+                       mBottomNavigationView.getMenu().findItem(R.id.action_presstime).setChecked(true);
+                       break;
+               }
+           }
+       });
+
+       //TIME
+
+
+    }
+}
---
+++ app/src/main/java/com/example/flashclock/ViewPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.example.flashclock;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new TimerFragment();
+
+            case 1:
+                return new TimenationFragment();
+
+            case 2:
+                return new PresstimeFragment();
+
+            default:
+                return new TimerFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
